5ef8071452ac2a86b70fb26cacf6837fb60380b3
Cyber <ravshanbekilhamov@gmail.com>
2024-10-18 11:32:25 +0500

    "Update"

--- App/Model/Model.php
+++ App/Model/Model.php
@@ -1,113 +1,5 @@
 <?php
 
-// class Model extends Database {
-    
-//     public static function getAll() : array 
-//     {
-    //         $con = self::connect(); 
-    //         try {
-        //             $query = "SELECT * FROM " . static::$table_name;
-        
-        //             $stmt = $con->prepare($query);
-        //             $stmt->execute();
-//             return $stmt->fetchAll(PDO::FETCH_ASSOC);
-            
-//         } catch (PDOException $e) {
-    //             echo $e->getMessage();
-    //         }
-    //     }
-    
-    //     public static function delete($id){
-        //         try{
-//             $query = "DELETE FROM " . static::$table_name . " WHERE id = :id"; 
-            
-//             $stmt = self::connect()->prepare($query);
-//             $stmt->bindParam(':id', $id, PDO::PARAM_INT);
-//             $stmt->execute();
-
-//             return true;
-
-//         }catch(PDOException $e){
-    //             return false;
-    //         } 
-    //     }
-    
-    //     public static function edit($new_name,$id){
-        
-    //         try{    
-//             $query = "UPDATE " .  static::$table_name . " SET name = '{$new_name}' WHERE id = '{$id}'";
-
-//             $stmt = self::connect()->prepare($query);
-//             $stmt->execute();
-//             return true;
-
-//         }catch(PDOException $e){
-    //             return false;
-    
-    //         }
-    //     }
-
-    //     public static function create($name) {
-        //         try {
-            //             $query = "INSERT INTO " . static::$table_name . " (name) VALUES (:name)";
-            
-            //             $stmt = self::connect()->prepare($query);
-            
-            //             $stmt->bindParam(':name', $name, PDO::PARAM_STR);
-            
-            //             if ($stmt->execute()) {
-//                 return true;
-//             } else {
-    //                 $errorInfo = $stmt->errorInfo();
-//                 echo "Error executing query: " . $errorInfo[2];
-//                 return false;
-//             }
-
-//         } catch (PDOException $e) {
-    //             echo "Database error: " . $e->getMessage();
-    //             return false;
-    //         }
-    //     }
-    
-    //     public static function register_user($data){
-        
-    //         $values = '';
-    //         foreach($data as $key=>$value){
-        //             if($key =='password'){
-            //                 $value = md5(($value));
-            //             }
-            //             $values .= "'{$value}' , ";
-            //         }
-            
-            //         $cleanString = rtrim($values,", ");
-            //         // dd($cleanString);
-            //         $conn = self::connect();
-//         $stmt = $conn->prepare("INSERT INTO users (name,email,password) VALUES (" . $cleanString . ")");
-//         return $stmt->execute();
-
-//     }
-//     public static function get_user($data){
-
-//         $values = '';
-//         foreach($data as $key=>$value){
-    //             if($key =='password'){
-//                 $value = md5(($value));
-//             }
-//             $values .= "{$key} = '{$value}' AND ";
-//         }
-
-//         $cleanString = rtrim($values,"AND ");
-//         // dd($cleanString);
-//         $conn = self::connect();
-//         $stmt = $conn->prepare("SELECT * FROM users WHERE (" . $cleanString . ")");
-//         $stmt->execute();
-//         return $stmt->fetch(PDO::FETCH_ASSOC);
-
-//     }
-
-// }
-
-
 namespace App\Model;
 
 use PDO;
@@ -123,10 +15,10 @@ interface Form {
 }
 
 abstract class Model implements Form {
-    protected static $conn;  // Define static property for connection
+    protected static $conn; 
 
     public static function connect() {
-        if (self::$conn === null) {  // Only create a new connection if none exists
+        if (self::$conn === null) {  
             try {
                 self::$conn = new PDO('mysql:host=localhost;dbname=lesson_18_db', 'root', 'Cyberboy@5');
                 self::$conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
